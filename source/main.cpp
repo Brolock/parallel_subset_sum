e78979bc8cbabf07565845b0bf53fdad09cc413a
Francois Karnad Dziedzinl <francois.dziedzinl@epita.fr>
2015-07-30 12:55:25 +0200

    pretty printing lesson 1

--- source/main.cpp
+++ source/main.cpp
@@ -7,14 +7,6 @@
 
 int main()
 {
-    // 1D array
-    auto array = tools::generate_array<int>(10);
-
-    tools::print_vector(array);
-
-    auto sub_array = get_max_subarray(array);
-    //tools::print_vector(sub_array);
-
     // 2D array
     auto array_2D = tools::generate_2D_array<int>(1000);
     //tools::print_vector(array_2D);

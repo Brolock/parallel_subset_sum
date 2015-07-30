8eeb56e303f34db5f9bf53a0491438f794683a8c
Marcelin Dupraz <marcelin.dupraz@gmail.com>
2015-07-30 13:08:59 +0200

    Olala on prends les argc

--- source/main.cpp
+++ source/main.cpp
@@ -4,23 +4,25 @@
 
 #include "parallel_compute.h"
 
-
-int main()
+int main(int argc, const char *argv[])
 {
     // 2D array
-    auto array_2D = tools::generate_2D_array<int>(1000);
-    //tools::print_vector(array_2D);
+    if (argc < 2)
+        return -1;
+
+    auto array_2D = tools::generate_2D_array<int>(atoi(argv[1]));
+    tools::print_vector(array_2D);
 
     auto sub_array_2D = get_max_subarray(array_2D);
 
-    //tools::print_vector(sub_array_2D);
+    tools::print_vector(sub_array_2D);
 
     //Parallel 2D
     //auto parallel_array = tools::generate_2D_array<int>(4);
     
     auto sub_thread = parallel_max_subarray(array_2D);
 
-    //tools::print_vector(sub_thread);
+    tools::print_vector(sub_thread);
 
     return 0;
 }
--- source/printer.h
+++ source/printer.h
@@ -9,10 +9,10 @@ namespace tools
 template<typename T>
 void print_vector(const std::vector<T>& vec)
 {
-    std::cout << "[ ";
+    std::cerr << "[ ";
     std::for_each(vec.begin(), vec.end(),
-            [](const T& value){ std::cout << value << " "; });
-    std::cout << "]" << std::endl;
+            [](const T& value){ std::cerr << value << " "; });
+    std::cerr << "]" << std::endl;
 }
 
 template<typename T>
@@ -20,26 +20,26 @@ void print_vector(const std::vector<std::vector<T>>& vec)
 {
     if (vec.size() == 0)
     {
-        std::cout << "No sub_array found" << std::endl;
+        std::cerr << "No sub_array found" << std::endl;
         return;
     }
-    std::cout << "[\n";
+    std::cerr << "[\n";
     for (unsigned j = 0; j < vec[0].size(); ++j)
     {
-        std::cout << "{ ";
+        std::cerr << "{ ";
         for (unsigned i = 0; i < vec.size(); ++i)
         {
             // Boring checking to have a nice alignement in the printing
             if (vec[i][j] >= 0 && vec[i][j] < 10)
-                std::cout << "  ";
+                std::cerr << "  ";
             else if (vec[i][j] >= 10 || (vec[i][j] < 0 && vec[i][j] > -10))
-                std::cout << " ";
+                std::cerr << " ";
 
 
-            std::cout << vec[i][j] << " ";
+            std::cerr << vec[i][j] << " ";
         }
-        std::cout << "}\n";
+        std::cerr << "}\n";
     }
-    std::cout << "]" << std::endl;
+    std::cerr << "]" << std::endl;
 }
 }
